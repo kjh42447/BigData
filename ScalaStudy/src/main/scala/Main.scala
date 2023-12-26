542d4191ae26026ec3c51dfd90354ea0838b3e07
kjh42447 <kjh42447@gmail.com>
2023-12-27 08:55:38 +0900

    scala 학습 시작

---
+++ ScalaStudy/src/main/scala/Main.scala
@@ -0,0 +1,63 @@
+// 빌드 및 실행 : ctrl + shift + r
+// 주석 : command + /
+
+object Main {
+  def main(args: Array[String]): Unit = {
+//    객체
+//    Any - (AnyVal, AnyRef) - 대상 자료형 상속
+//    val s1 = "Hello"
+//    val s2 = "HeLLo"
+//    val s3 = "Hello"
+//
+//    case class Person( p1:String, p2:String )
+//    val p1 = Person("a", "b")
+//    val p2 = Person("a", "b")
+//    val p3 = Person("b", "c")
+//
+//    println(1 == 1)
+//    println(1 == 'a')
+//    println('a' == 'a')
+//    println(s1 == s2)
+//    println(s1 == s3)
+//    println(p1 == p2)
+//    println(p1 == p3)
+
+//    자료형
+//    암시적 선언
+//    var x = 10
+//    var y = "abc"
+//    var z =
+//      """a
+//        |b
+//        |c
+//        |""".stripMargin
+//
+//    println(x)
+//    println(y)
+//    println(z)
+//
+////    명시적 선언
+//    var b: Byte = 10
+//    var s: Short = 10
+//    var i: Int = 10
+//    var l: Long = 10
+//    var d: Double = 12.34
+//    var str1: String = "a\n" +
+//      "b" +
+//      "c"
+//
+//    println(str1)
+//
+//    println(s"${ str1 }")
+//    println(s"${ 1 + 1 }")
+//    println(f"$y%s is $d%2.2f meters tall")
+//    println(raw"a\nbc")
+
+//    변수
+//    var : 가변
+//    val : 불변
+//    불변 값이면 불변 변수를 사용하는 것을 선호(동시성 이슈)
+
+//    함수
+  }
+}
